6c6aad86f437349e9bcbf47a982a98dc0e965f70
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2022-06-22 22:48:13 +0200

    Added register language files

--- lang/nl/register.php
+++ lang/nl/register.php
@@ -2,4 +2,13 @@
 
 return array (
   'title' => 'Registeren',
+  'page_title' => 'Registreer een account',
+  'form_name' => 'Naam',
+  'form_email' => 'Email',
+  'form_password' => 'Wachtwoord',
+  'form_password_confirm' => 'Bevestig wachtwoord',
+  'form_submit' => 'Registreren',
+  'accept_tos' => 'Accepteer privacy voorwaarden en ToS',
+  'has_account_text' => 'Al een account?',
+  'has_account_button' => 'Log in',
 );
