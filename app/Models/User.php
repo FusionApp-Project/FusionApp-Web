74be5f9746d22763c693467c65d2f78f19cba1b8
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2022-06-22 22:47:47 +0200

    Added DiscordAccount relationship to User class

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function discordAccount()
+    {
+        return $this->hasOne(DiscordAccount::class);
+    }
 }
