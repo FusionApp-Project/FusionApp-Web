35a3707091a0d4919409cdd735a6120a666439a9
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2022-07-19 16:04:47 +0200

    Add sessions migration (#9)

---
+++ database/migrations/2022_07_19_135400_create_sessions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id', false, true)->unsigned()->index();
+            $table->string('access_token', 32)->unique();
+            $table->string('refresh_token', 32)->unique();
+            $table->dateTime('expires');
+            $table->string('code', 32)->unique()->nullable();
+
+            // Not needed
+            // $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
